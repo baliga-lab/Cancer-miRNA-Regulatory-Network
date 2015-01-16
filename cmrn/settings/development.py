39fa0f030c0ae61fd592c9e6cf4b7956692bfc8f
Wei-ju Wu <weiju.wu@gmail.com>
2015-01-16 10:29:55 -0800

    bugfix: Django deprecation
    1.6 eliminates the django.con.urls.defaults package, fixed this

--- cmrn/settings/development.py
+++ cmrn/settings/development.py
@@ -1,1 +1,21 @@
 from .base import *
+import sys
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
